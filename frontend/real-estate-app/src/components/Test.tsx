a21c13df96a28946d8352ae76e68ec6f0c0a1a4f
Amiel De Los Reyes <amieldelosreyes@Amiels-MacBook-Pro.local>
2025-05-26 23:44:33 -0600

    Inital project structure

---
+++ frontend/real-estate-app/src/components/Test.tsx
@@ -0,0 +1,11 @@
+export const Test = () => {
+    return (
+        <>
+            <div>
+                <h2>HELLO FROM TEST!</h2>
+            </div>
+        </>
+    )
+}
+
+export default Test;
